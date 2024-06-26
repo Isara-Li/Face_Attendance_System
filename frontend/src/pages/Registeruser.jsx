c85cc86e36f8cb239608b4deab9f35abab746372
Isara-Li <vimandika.21@cse.mrt.ac.lk>
2024-06-26 21:24:14 +0530

    Update Registeruser.jsx

--- frontend/src/pages/Registeruser.jsx
+++ frontend/src/pages/Registeruser.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Appbar from '../components/Appbar';
 
 const Registeruser = () => {
     const [newUsername, setNewUsername] = useState('');
@@ -52,46 +53,49 @@ const Registeruser = () => {
     };
 
     return (
-        <div className="flex justify-center items-center h-screen bg-gray-900">
-            <div className="bg-black bg-opacity-80 rounded-2xl p-0 m-2 h-96 w-96">
-                <form onSubmit={handleSubmit} className="flex flex-col items-center">
-                    <h2 className="bg-pink-600 text-white rounded-t-2xl py-2 px-4 w-full text-center">
-                        Add New User <i className="material-icons">control_point_duplicate</i>
-                    </h2>
-                    <label className="text-white text-lg mt-4">
-                        <b>Enter New User Name*</b>
-                    </label>
-                    <input
-                        type="text"
-                        id="newusername"
-                        name="newusername"
-                        value={newUsername}
-                        onChange={(e) => setNewUsername(e.target.value)}
-                        className="text-lg mt-2 mb-2 rounded-lg w-72 h-12 px-2"
-                        required
-                    />
-                    <label className="text-white text-lg">
-                        <b>Enter New User Id*</b>
-                    </label>
-                    <input
-                        type="number"
-                        id="newuserid"
-                        name="newuserid"
-                        value={newUserId}
-                        onChange={(e) => setNewUserId(e.target.value)}
-                        className="text-lg mt-2 mb-2 rounded-lg w-72 h-12 px-2"
-                        required
-                    />
-                    <button
-                        type="submit"
-                        className="bg-gray-800 text-white text-lg rounded-lg w-72 h-12 mt-4"
-                    >
-                        Add New User
-                    </button>
-                    <h5 className="text-white py-6">
-                        <i>Total Users in Database: {totalUsers}</i>
-                    </h5>
-                </form>
+        <div className="bg-[#c9c7c7] p-4">
+            <Appbar />
+            <div className="flex justify-center items-center h-screen bg-[#c9c7c7]">
+                <div className="bg-black bg-opacity-80 rounded-2xl p-0 m-2 h-96 w-96">
+                    <form onSubmit={handleSubmit} className="flex flex-col items-center">
+                        <h2 className="bg-pink-600 text-white rounded-t-2xl py-2 px-4 w-full text-center">
+                            Add New User
+                        </h2>
+                        <label className="text-white text-lg mt-4">
+                            <b>Enter New User Name</b>
+                        </label>
+                        <input
+                            type="text"
+                            id="newusername"
+                            name="newusername"
+                            value={newUsername}
+                            onChange={(e) => setNewUsername(e.target.value)}
+                            className="text-lg mt-2 mb-2 rounded-lg w-72 h-12 px-2"
+                            required
+                        />
+                        <label className="text-white text-lg">
+                            <b>Enter New User Id</b>
+                        </label>
+                        <input
+                            type="number"
+                            id="newuserid"
+                            name="newuserid"
+                            value={newUserId}
+                            onChange={(e) => setNewUserId(e.target.value)}
+                            className="text-lg mt-2 mb-2 rounded-lg w-72 h-12 px-2"
+                            required
+                        />
+                        <button
+                            type="submit"
+                            className="bg-gray-800 text-white text-lg rounded-lg w-72 h-12 mt-4"
+                        >
+                            Add New User
+                        </button>
+                        <h5 className="text-white py-6">
+                            <i>Total Users in Database: {totalUsers}</i>
+                        </h5>
+                    </form>
+                </div>
             </div>
         </div>
     );
